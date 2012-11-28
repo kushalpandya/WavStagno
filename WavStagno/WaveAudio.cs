762036b83349949dc22a3b82f8e05fd31c823487
Kushal Pandya <kushal.pandya04@gmail.com>
2012-11-29 03:23:38 +0530

    Wave audio file representation object.

---
+++ WavStagno/WaveAudio.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WavStagno
+{
+    namespace Media
+    {
+        class WaveAudio
+        {
+            private byte[] riffID; // "riff"
+            private uint size;  // Size
+            private byte[] wavID;  // Format
+            private byte[] fmtID;  // Subchunk1ID
+            private uint fmtSize; // Subchunk size
+            private ushort format; // format
+            private ushort channels; // no of channels
+            private uint sampleRate; // Samplerate
+            private uint bytePerSec;
+            private ushort blockSize;
+            private ushort bit;
+            private byte[] dataID;// "data"
+            private uint dataSize;
+        }
+    }
+}
