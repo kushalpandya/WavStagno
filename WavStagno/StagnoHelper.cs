a9dde33be8cb1734028e26c6ad784a7d189e61e5
Kushal Pandya <kushal.pandya04@gmail.com>
2012-11-29 03:23:16 +0530

    Class to perform Stagnography on Wave Audio file.

---
+++ WavStagno/StagnoHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WavStagno.Media;
+
+namespace WavStagno
+{
+    class StagnoHelper
+    {
+        public void hideMessage(WaveAudio file, String message)
+        {
+
+        }
+
+        public String extractMessage(WaveAudio file)
+        {
+            return "Hello";
+        }
+    }
+}
