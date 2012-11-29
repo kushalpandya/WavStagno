700ea4fbfd363e39fc7d246d114ef97794638f85
Kushal Pandya <kushal.pandya04@gmail.com>
2012-11-29 16:14:33 +0530

    Methods added to hide and extract message into WaveAudio object.

--- WavStagno/StagnoHelper.cs
+++ WavStagno/StagnoHelper.cs
@@ -5,17 +5,49 @@ using System.Text;
 using System.Threading.Tasks;
 using WavStagno.Media;
 
+/// <summary>
+/// WavStagno Main Namespace.
+/// </summary>
 namespace WavStagno
 {
+    /// <summary>
+    /// Stagnography helper class for Hiding and Extracting message in WaveAudio object.
+    /// </summary>
     class StagnoHelper
     {
-        public void hideMessage(WaveAudio file, String message)
-        {
+        private WaveAudio file;
 
+        /// <summary>
+        /// Initializes this StagnoHelper object with WaveAudio object.
+        /// </summary>
+        /// <param name="file">WaveAudio object to be initilized.</param>
+        public StagnoHelper(WaveAudio file)
+        {
+            this.file = file;
         }
 
-        public String extractMessage(WaveAudio file)
+        /// <summary>
+        /// Hides a message in WaveAudio object's Left and Right Audio channels.
+        /// </summary>
+        /// <param name="message">String Message to be hidden.</param>
+        public void HideMessage(string message)
         {
+            List<short> leftStream = file.GetLeftStream();
+            List<short> rightStream = file.GetRightStream();
+
+            //Hide Message in Streams and call file.UpdateStreams(leftStream, rightStream)
+        }
+
+        /// <summary>
+        /// Extracts message from WaveAudio object's Left and Right Audio channels.
+        /// </summary>
+        /// <returns>String representing Extracted Message.</returns>
+        public string ExtractMessage()
+        {
+            List<short> leftStream = file.GetLeftStream();
+            List<short> rightStream = file.GetRightStream();
+
+            //Extract Message from Streams and Return it.
             return "Hello";
         }
     }
